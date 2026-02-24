66ae2966885f76437c43a85e7dfdb771c897ee59
taperihn0 <szymonstar690@gmail.com>
2026-02-24 17:13:53 +0100

    core fork logic

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,219 +1,9 @@
-#define _XOPEN_SOURCE
-
+#include "core/pty.h" 
+#include "core/tty_man.h"
 #include "Defines.hpp"
 #include "Assert.hpp"
 
-#include <iostream>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-#include <termios.h>
-#include <signal.h>
-#include <poll.h>
-
-#ifndef _HAS_OPENPT
-int posix_openpt(int oflag)
-{
-   int fdm;
-   fdm = open("/dev/ptmx", oflag);
-   return fdm;
-}
-#endif
-
-#ifndef _HAS_PTSNAME
-char* ptsname(int fdm)
-{
-   int sminor;
-   static char pts_name[16];
-
-   if (ioctl(fdm, TIOCGPTN, &sminor) < 0)
-      return NULL;
-
-   snprintf(pts_name, sizeof(pts_name), "/dev/pts/%d", sminor);
-   return pts_name;
-}
-#endif
-
-#ifndef _HAS_GRANTPT
-int grantpt(int fdm)
-{
-   char *pts_name;
-   pts_name = ptsname(fdm);
-   return chmod(pts_name, S_IRUSR | S_IWUSR | S_IWGRP);
-}
-#endif
-
-#ifndef _HAS_UNLOCKPT
-int unlockpt(int fdm)
-{
-   int lock = 0;
-   return ioctl(fdm, TIOCSPTLCK, &lock);
-}
-#endif
-
-int ptym_open(char *pts_name, int pts_namesz)
-{
-   char *ptr;
-   int fdm;
-
-   /*
-   * Return the name of the master device so that on failure
-   * the caller can print an error message. Null terminate
-   * to handle case where string length > pts_namesz.
-   */
-   strncpy(pts_name, "/dev/ptmx", pts_namesz);
-   pts_name[pts_namesz - 1] = '\0';
-
-   if ((fdm = posix_openpt(O_RDWR)) < 0)
-      return -1;
-
-   if (grantpt(fdm) < 0) { /* grant access to slave */
-      close(fdm);
-      return -2;
-   }
-
-   if (unlockpt(fdm) < 0) { /* clear slave's lock flag */
-      close(fdm);
-      return -3;
-   }
-   
-   if ((ptr = ptsname(fdm)) == NULL) { /* get slave's name */
-      close(fdm);
-      return -4;
-   }
-
-   /*
-   * Return name of slave. Null terminate to handle case
-   * where strlen(ptr) > pts_namesz.
-   */
-   strncpy(pts_name, ptr, pts_namesz);
-   pts_name[pts_namesz - 1] = '\0';
-
-   return fdm; /* return fd of master */
-}
-
-int ptys_open(char *pts_name)
-{
-   int fds;
-
-   if ((fds = open(pts_name, O_RDWR)) < 0)
-      return -5;
-
-   return fds;
-}
-
-pid_t pty_fork(int* ptrfdm, char* slave_name, int slave_namesz,
-               const struct termios* slave_termios,
-               const struct winsize* slave_winsize)
-{
-   int fdm, fds;
-   pid_t pid;
-   char pts_name[20];
-
-   if ((fdm = ptym_open(pts_name, sizeof(pts_name))) < 0) {
-      THR_HARD_ASSERT_LOG(false, "Failed to open master pty");
-   }
-
-   if (slave_name != NULL) {
-      /*
-      * Return name of slave. Null terminate to handle case
-      * where strlen(pts_name) > slave_namesz.
-      */
-      strncpy(slave_name, pts_name, slave_namesz);
-      slave_name[slave_namesz - 1] = '\0';
-   }
-
-   if ((pid = fork()) < 0) {
-      return -1;
-   } 
-   else if (pid == 0) { /* child */
-      if (setsid() < 0)
-         fprintf(stderr, "setsid error");
-         
-      /*
-      * System V acquires controlling terminal on open().
-      */
-      if ((fds = ptys_open(pts_name)) < 0)
-         fprintf(stderr, "Can't open slave pty");
-
-      close(fdm); /* all done with master in child */
-      
-#if defined(TIOCSCTTY)
-      /*
-      * TIOCSCTTY is the BSD way to acquire a controlling terminal.
-      */
-      if (ioctl(fds, TIOCSCTTY, nullptr) < 0)
-         fprintf(stderr, "TIOCSCTTY error");
-#endif
-
-      /*
-      * Set slave's termios and window size.
-      */
-      if (slave_termios != NULL) {
-         if (tcsetattr(fds, TCSANOW, slave_termios) < 0)
-            fprintf(stderr, "tcsetattr error on slave pty");
-      }
-
-      if (slave_winsize != NULL) {
-         if (ioctl(fds, TIOCSWINSZ, slave_winsize) < 0)
-            fprintf(stderr, "TIOCSWINSZ error on slave pty");
-      }
- 
-      /*
-      * Slave becomes stdin/stdout/stderr of child.
-      */
-      if (dup2(fds, STDIN_FILENO) != STDIN_FILENO)
-         fprintf(stderr, "dup2 error to stdin");
-
-      if (dup2(fds, STDOUT_FILENO) != STDOUT_FILENO)
-         fprintf(stderr, "dup2 error to stdout");
-      
-      if (dup2(fds, STDERR_FILENO) != STDERR_FILENO)
-         fprintf(stderr, "dup2 error to stderr");
-
-      if (fds != STDIN_FILENO && fds != STDOUT_FILENO && fds != STDERR_FILENO)
-         close(fds);
-
-      return 0; /* child returns 0 just like fork() */
-   } 
-   else { /* parent */
-      *ptrfdm = fdm; /* return fd of master */
-      return pid; /* parent returns pid of child */
-   }
-}
-
 #define BUFFSIZE 512
-
-static void sig_term(int);
-static volatile sig_atomic_t sigcaught; /* set by signal handler */
-
-int writen(int fd, const void* buff, int n)
-{
-   ssize_t written = 0;
-   ssize_t left = n;
-
-   const char* ptr = (const char*)buff;
-
-   while (left > 0) {
-      if ((written = write(fd, ptr, left)) < 0) {
-         if (left == n)
-            return -1;
-         else 
-            break;
-      } 
-      else if (written == 0) {
-         break;
-      }
-
-      ptr += written;
-      left -= written;
-   }
-
-   return n - left;
-}
 
 void loop(int ptym)
 {
@@ -257,158 +47,29 @@ void loop(int ptym)
    */
 }
 
-/*
- * The child sends us SIGTERM when it gets EOF on the pty slave or
- * when read() fails. We probably interrupted the read() of ptym.
- */
-static void sig_term(int)
-{
-   printf("Catching SIGTERM signal");
-   sigcaught = 1; /* just set flag and return */
+struct termios orig_termios;
+struct winsize size;
+
+void tty_atexit() {
+   tty_set(STDIN_FILENO, &orig_termios, &size);
 }
 
-static void set_noecho(int fd) /* turn off echo (for slave pty) */
-{
-   struct termios stermios;
-
-   if (tcgetattr(fd, &stermios) < 0)
-      fprintf(stderr, "tcgetattr error");
-   
-   stermios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
-   
-   /*
-   * Also turn off NL to CR/NL mapping on output.
-   */
-   //stermios.c_oflag &= ~ONLCR;
-
-   if (tcsetattr(fd, TCSANOW, &stermios) < 0)
-      fprintf(stderr, "tcsetattr error");
-}
-
-static struct termios save_termios;
-static int ttysavefd = -1;
-
-enum ttyState { 
-   RESET, 
-   RAW,
-}; 
-
-static ttyState ttystate = RESET;
-
-int tty_raw(int fd) /* put terminal into a raw mode */
-{
-   int err;
-   struct termios buf;
-
-   if (ttystate != RESET) {
-      errno = EINVAL;
-      return(-1);
-   }
-
-   if (tcgetattr(fd, &buf) < 0)
-      return(-1);
-
-   save_termios = buf; /* structure copy */
-   /*
-   * Echo off, canonical mode off, extended input
-   * processing off, signal chars off.
-   */
-   buf.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-   /*
-   * No SIGINT on BREAK, CR-to-NL off, input parity
-   * check off, don't strip 8th bit on input, output
-   * flow control off.
-   */
-   buf.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-   /*
-   * Clear size bits, parity checking off.
-   */
-   buf.c_cflag &= ~(CSIZE | PARENB);
-   /*
-   * Set 8 bits/char.
-   */
-   buf.c_cflag |= CS8;
-   /*
-   * Output processing off.
-   */
-   buf.c_oflag &= ~(OPOST);
-   /*
-   * Case B: 1 byte at a time, no timer.
-   */
-   buf.c_cc[VMIN] = 1;
-   buf.c_cc[VTIME] = 0;
-
-   if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
-      return(-1);
-
-   /*
-   * Verify that the changes stuck. tcsetattr can return 0 on
-   * partial success.
-   */
-   if (tcgetattr(fd, &buf) < 0) {
-      err = errno;
-      tcsetattr(fd, TCSAFLUSH, &save_termios);
-      errno = err;
-      return(-1);
-   }
-   
-   if ((buf.c_lflag & (ECHO | ICANON | IEXTEN | ISIG)) ||
-       (buf.c_iflag & (BRKINT | ICRNL | INPCK | ISTRIP | IXON)) ||
-       (buf.c_cflag & (CSIZE | PARENB | CS8)) != CS8 ||
-       (buf.c_oflag & OPOST) || buf.c_cc[VMIN] != 1 ||
-        buf.c_cc[VTIME] != 0) {
-      /*
-      * Only some of the changes were made. Restore the
-      * original settings.
-      */
-      tcsetattr(fd, TCSAFLUSH, &save_termios);
-      errno = EINVAL;
-      return(-1);
-   }
-
-   ttystate = RAW;
-   ttysavefd = fd;
-
-   return(0);
-}
-
-int tty_reset(int fd) /* restore terminal's mode */
-{
-   if (ttystate == RESET)
-      return(0);
-   
-   if (tcsetattr(fd, TCSAFLUSH, &save_termios) < 0)
-      return(-1);  
-
-   ttystate = RESET;
-   return(0);
-}
-
-void tty_atexit() /* can be set up by atexit(tty_atexit) */
-{
-   if (ttysavefd >= 0)
-      tty_reset(ttysavefd);
-}
-
-int main(int argc, char* argv[])
+int main()
 {
    int fdm, interactive, noecho, verbose;
    pid_t pid;
    char slave_name[20];
-
-   struct termios orig_termios;
-   struct winsize size;
 
    interactive = isatty(STDIN_FILENO);
    noecho = 0;
    verbose = 1;
 
    if (interactive) { /* fetch current termios and window size */
-      if (tcgetattr(STDIN_FILENO, &orig_termios) < 0)
-         fprintf(stderr, "tcgetattr error on stdin");
+      if (save_termios(STDIN_FILENO, &orig_termios) < 0)
+         fprintf(stderr, "Failed to save origin termios attributes");
 
-      if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char*)&size) < 0)
-         fprintf(stderr, "TIOCGWINSZ error");
+      if (save_winsize(STDIN_FILENO, &size) < 0)
+         fprintf(stderr, "Failed to save origin winsize");
 
       pid = pty_fork(&fdm, slave_name, sizeof(slave_name),
                      &orig_termios, &size);
---
+++ src/core/tty_man.c
@@ -0,0 +1,129 @@
+#include "tty_man.h"
+#include <errno.h>
+
+int save_termios(int fd, struct termios* termios) {
+   struct termios stermios;
+
+   if (tcgetattr(fd, &stermios) < 0) {
+      fprintf(stderr, "Failed to get termios attributes on fd = %d", fd);
+      return TERMIOS_GETATTR_ERR;
+   }
+
+   *termios = stermios;
+   return 0;
+}  
+
+int save_winsize(int fd, struct winsize* winsize) {
+   struct winsize swinsize;
+
+   if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char*)&swinsize) < 0) {
+      fprintf(stderr, "Failed to get winsize attribute on fd = %d", fd);
+      return WINSIZE_GETATTR_ERR;
+   }
+
+   return 0;
+}
+
+void set_noecho(int fd)
+{
+   struct termios stermios;
+
+   if (tcgetattr(fd, &stermios) < 0)
+      fprintf(stderr, "tcgetattr error");
+   
+   stermios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
+   
+   /*
+   * Also turn off NL to CR/NL mapping on output.
+   */
+   stermios.c_oflag &= ~ONLCR;
+
+   if (tcsetattr(fd, TCSANOW, &stermios) < 0)
+      fprintf(stderr, "tcsetattr error");
+}
+
+int tty_raw(int fd)
+{
+   int err;
+   struct termios buf, stermios;
+
+   if ((err = save_termios(fd, &buf)) < 0)
+      return err;
+
+   stermios = buf;
+
+   /*
+   * Echo off, canonical mode off, extended input
+   * processing off, signal chars off.
+   */
+   buf.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+   /*
+   * No SIGINT on BREAK, CR-to-NL off, input parity
+   * check off, don't strip 8th bit on input, output
+   * flow control off.
+   */
+   buf.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+   /*
+   * Clear size bits, parity checking off.
+   */
+   buf.c_cflag &= ~(CSIZE | PARENB);
+   /*
+   * Set 8 bits/char.
+   */
+   buf.c_cflag |= CS8;
+   /*
+   * Output processing off.
+   */
+   buf.c_oflag &= ~(OPOST);
+   /*
+   * Case B: 1 byte at a time, no timer.
+   */
+   buf.c_cc[VMIN] = 1;
+   buf.c_cc[VTIME] = 0;
+
+   if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+      return TERMIOS_SETATTR_ERR;
+
+   /*
+   * Verify that the changes stuck. tcsetattr can return 0 on
+   * partial success.
+   */
+   if (save_termios(fd, &buf) < 0) {
+      err = errno;
+      tcsetattr(fd, TCSAFLUSH, &stermios);
+      errno = err;
+      return TERMIOS_SETATTR_ERR;
+   }
+   
+   if ((buf.c_lflag & (ECHO | ICANON | IEXTEN | ISIG)) ||
+       (buf.c_iflag & (BRKINT | ICRNL | INPCK | ISTRIP | IXON)) ||
+       (buf.c_cflag & (CSIZE | PARENB | CS8)) != CS8 ||
+       (buf.c_oflag & OPOST) || buf.c_cc[VMIN] != 1 ||
+        buf.c_cc[VTIME] != 0) {
+      /*
+      * Only some of the changes were made. Restore the
+      * original settings.
+      */
+      tcsetattr(fd, TCSAFLUSH, &stermios);
+      errno = EINVAL;
+      return TERMIOS_SETATTR_ERR;
+   }
+
+   return 0;
+}
+
+int tty_set(int fd, struct termios* termios, struct winsize* winsize) {
+   if (termios != NULL && 
+       tcsetattr(fd, TCSAFLUSH, termios) < 0) {
+      fprintf(stderr, "Failed to set termios attributes on fd = %d", fd);
+      return TERMIOS_SETATTR_ERR;
+   }
+
+   if (winsize != NULL && 
+       ioctl(fd, TIOCSWINSZ, winsize) < 0) {
+      fprintf(stderr, "Failed to set winsize attribute on fd = %d", fd);
+      return WINSIZE_SETATTR_ERR;
+   }
+
+   return 0;
+}
